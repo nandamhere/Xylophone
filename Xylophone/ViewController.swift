a0b2a3f5408a1fc9dc120485e335a46f2940092e
Vaibhav Nandam <vaibhavnandam@vaibhavs-air.home>
2018-07-05 12:27:36 -0400

    added constraints and functionality to the buttons

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var playSound : AVAudioPlayer?
+    
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+   
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let path = Bundle.main.path(forResource: soundArray[sender.tag], ofType: "wav")
+        let url = URL(fileURLWithPath: path!)
+        
+        do {
+            playSound = try AVAudioPlayer(contentsOf: url)
+            playSound?.play()
+        } catch {
+            print("\(error)")
+        }
+    }
+    
 }
 
